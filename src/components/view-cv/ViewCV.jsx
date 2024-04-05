2096943257ddc053a3bb023230597741fcf26d02
Alexander Yosimar Cervantes Pineda <aycp.developer@outlook.com>
2024-04-05 14:50:42 -0600

    ThemeContext agregado con su estado en el componente view-cv

--- src/components/view-cv/ViewCV.jsx
+++ src/components/view-cv/ViewCV.jsx
@@ -1,13 +1,13 @@
 import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import './ViewCV.css';
-import { Context } from '../../exports/exports';
+import { ThemeContext } from '../../exports/exports';
 import { EyeFill, X, ZoomIn, ZoomOut } from 'react-bootstrap-icons';
 
 const ViewCV = ({ backgroundColor, color, cvImage }) => {
 
-    const context = useContext(Context);
-    const themeState = context.state.themeState;
+    const context = useContext(ThemeContext);
+    const themeState = context.themeState.themeState;
     const [cvFileWindow, setCvFileWindow] = useState(false);
     const [scale, setScale] = useState(1);
     const [marginAtTheTopOfTheImage, setMarginAtTheTopOfTheImage] = useState(null);
