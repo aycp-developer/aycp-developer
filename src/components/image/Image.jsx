c31519cef7b1c37dea6e4d00c76d990fc61bc9b1
Alexander Yosimar Cervantes Pineda <aycp.developer@outlook.com>
2024-04-05 14:43:26 -0600

    ThemeContext agregado con su estado en el componente image

--- src/components/image/Image.jsx
+++ src/components/image/Image.jsx
@@ -1,12 +1,12 @@
 import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import './Image.css';
-import { Context } from '../../exports/exports';
+import { ThemeContext } from '../../exports/exports';
 
 const Image = ({ boxShadow, image, alt }) => {
 
-    const context = useContext(Context);
-    const themeState = context.state.themeState;
+    const context = useContext(ThemeContext);
+    const themeState = context.themeState.themeState;
     const [imageLoaded, setImageLoaded] = useState(false);
     const shadowStyle = ((themeState == 'dark') ? '0 0 24px white' : '0 0 20px black');
 
