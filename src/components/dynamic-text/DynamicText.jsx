10a29e25c07e6dd9a0c4557e9ff85087d861081d
Alexander Yosimar Cervantes Pineda <aycp.developer@outlook.com>
2024-04-05 14:40:27 -0600

    ToggleNavbarContext y ThemeContext agregados con su estado en el componente dynamic-text

--- src/components/dynamic-text/DynamicText.jsx
+++ src/components/dynamic-text/DynamicText.jsx
@@ -1,13 +1,15 @@
 import React, { useContext, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './DynamicText.css';
-import { Context } from '../../exports/exports';
+import { ToggleNavbarContext, ThemeContext } from '../../exports/exports';
 import Typewriter from 'typewriter-effect/dist/core';
 
 const DynamicText = ({ strings, autoStart, loop, delay, deleteSpeed }) => {
 
-    const context = useContext(Context);
-    const themeState = context.state.themeState;
+    const toggleNavbarContext = useContext(ToggleNavbarContext);
+    const themeContext = useContext(ThemeContext);
+    const toggleNavbarState = toggleNavbarContext.toggleNavbarState.toggleNavbarState;
+    const themeState = themeContext.themeState.themeState;
     const textRef = useRef();
 
     useEffect(() => {
@@ -25,7 +27,10 @@ const DynamicText = ({ strings, autoStart, loop, delay, deleteSpeed }) => {
         <span
             className='dynamic-text'
             ref={textRef}
-            style={{color: (themeState == 'dark') ? 'white' : 'black'}}
+            style={{
+                display: toggleNavbarState ? 'none' : '',
+                color: (themeState == 'dark') ? 'white' : 'black'
+                }}
         />
     );
 
